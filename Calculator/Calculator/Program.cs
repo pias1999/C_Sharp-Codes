59ee7b01e7c90beaef04b19174c094fdf4d8c6ce
Ashikuzzaman Pias <33215174+pias1999@users.noreply.github.com>
2021-09-26 00:17:45 +0600

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int num1 = 0, num2;
+        char op;
+        Console.Write("Please enter the first integer: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        
+        for (; ;){
+            Console.WriteLine("To stop the code type 'e'. ");
+            Console.Write("Please enter an operand (+, -, *, /, ^): ");
+            op = Convert.ToChar(Console.ReadLine());
+            if(op == '+')
+            {
+                Console.Write("Please enter the second integer: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+                num1 = num1 + num2;
+                Console.WriteLine("Result: {0}", num1);
+            }else if (op == '-')
+            {
+                Console.Write("Please enter the second integer: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+                num1 = num1 - num2;
+                Console.WriteLine("Result: {0}", num1);
+            }else if (op == '*')
+            {
+                Console.Write("Please enter the second integer: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+                num1 = num1 * num2;
+                Console.WriteLine("Result: {0}", num1);
+            }else if (op == '/')
+            {
+                Console.Write("Please enter the second integer: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+                if (num2 == 0)
+                {
+                    Console.WriteLine("Cannot divide by zero.");
+                    Console.WriteLine("Current output: {0}", num1);
+                }
+                else
+                {
+                    num1 = num1 / num2;
+                    Console.WriteLine("Result: {0}", num1);
+                }
+            }
+            else if (op == '^')
+            {
+                Console.Write("Please enter the second integer: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+                num1 = (int)Math.Pow(num1, num2);
+                Console.WriteLine("Result: {0}", num1);
+            }else if(op == 'e')
+            {
+                Console.WriteLine("The code ENDED. ");
+                return;
+            }
+        }
+    }
+}
