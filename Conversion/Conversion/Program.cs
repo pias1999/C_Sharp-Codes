c376feed8f5e5eb4ae1333c4bdafef29af5c5a14
Ashikuzzaman Pias <33215174+pias1999@users.noreply.github.com>
2021-09-26 00:20:15 +0600

    Add files via upload

---
+++ Conversion/Conversion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        double num1, num2;
+
+        Console.Write("Enter first value: ");
+        num1 = double.Parse(Console.ReadLine());
+        Console.Write("Enter first value: ");
+        num2 = double.Parse(Console.ReadLine());
+        num1 = Convert.ToInt32(num1);
+        num2 = Convert.ToInt32(num2);
+        int sum = (int)(num1 + num2);
+        Console.WriteLine("Num1 = {0}, Num2 = {1}\nSum = {0} + {1} = {2}", num1, num2, sum);
+    }
+}
